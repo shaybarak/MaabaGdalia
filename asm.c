5b28d81a6b449300f58321ff3c355156d6a4f2ce
Noam <noaminbar510@gmail.com>
2013-03-11 19:43:22 +0200

    lies

--- asm.c
+++ asm.c
@@ -63,27 +63,27 @@ static void assemble_program(char *program_name)
 	asm_cmd(HLT, 0, 0, 0, 0);  // 8: Halt
 	*/
 	/* Multiplication program starts here */
-	asm_cmd(LD,  2, 0, 1, 1000);	// 0:  load multiplicand
-	asm_cmd(LD,  3, 0, 1, 1001);	// 1:  load multiplier
-	asm_cmd(ADD, 4, 0, 0, 0);		// 2:  sum = 0
-	asm_cmd(ADD, 6, 0, 0, 0);		// 3:  sign = positive
-	asm_cmd(JLT, 0, 0, 3, 7);		// 4:  if multiplier>0 goto loop
-	asm_cmd(SUB, 3, 0, 3, 0);		// 5:  negate multiplier
-	asm_cmd(ADD, 6, 0, 1, 1);		// 6:  sign = negative
-	// loop:
-	asm_cmd(JEQ, 0, 3, 0, 14);		// 7:  exit condition (multiplier == 0)
-	asm_cmd(AND, 5, 3, 1, 1);		// 8:  get multiplier lower bit
-	asm_cmd(JEQ, 0, 5, 0, 11);		// 9:  test multiplier lower bit
-	asm_cmd(ADD, 4, 4, 2, 0);		// 10:  sum += multiplicand
-	asm_cmd(LSF, 2, 2, 1, 1);		// 11:  multiplicand << 1
-	asm_cmd(RSF, 3, 3, 1, 1);		// 12: multiplier >> 1
-	asm_cmd(JEQ, 0, 0, 0, 7);		// 13: goto loop
-	// is_neg:
-	asm_cmd(JEQ, 0, 6, 0, 16);		// 14: if positive goto output
-	asm_cmd(SUB, 4, 0, 4, 0);		// 15: negate result
-	// output:
-	asm_cmd(ST,  0, 4, 1, 1002);	// 16: output result
-	asm_cmd(HLT, 0, 0, 0, 0);		// 17: halt
+	//asm_cmd(LD,  2, 0, 1, 1000);	// 0:  load multiplicand
+	//asm_cmd(LD,  3, 0, 1, 1001);	// 1:  load multiplier
+	//asm_cmd(ADD, 4, 0, 0, 0);		// 2:  sum = 0
+	//asm_cmd(ADD, 6, 0, 0, 0);		// 3:  sign = positive
+	//asm_cmd(JLT, 0, 0, 3, 7);		// 4:  if multiplier>0 goto loop
+	//asm_cmd(SUB, 3, 0, 3, 0);		// 5:  negate multiplier
+	//asm_cmd(ADD, 6, 0, 1, 1);		// 6:  sign = negative
+	//// loop:
+	//asm_cmd(JEQ, 0, 3, 0, 14);		// 7:  exit condition (multiplier == 0)
+	//asm_cmd(AND, 5, 3, 1, 1);		// 8:  get multiplier lower bit
+	//asm_cmd(JEQ, 0, 5, 0, 11);		// 9:  test multiplier lower bit
+	//asm_cmd(ADD, 4, 4, 2, 0);		// 10:  sum += multiplicand
+	//asm_cmd(LSF, 2, 2, 1, 1);		// 11:  multiplicand << 1
+	//asm_cmd(RSF, 3, 3, 1, 1);		// 12: multiplier >> 1
+	//asm_cmd(JEQ, 0, 0, 0, 7);		// 13: goto loop
+	//// is_neg:
+	//asm_cmd(JEQ, 0, 6, 0, 16);		// 14: if positive goto output
+	//asm_cmd(SUB, 4, 0, 4, 0);		// 15: negate result
+	//// output:
+	//asm_cmd(ST,  0, 4, 1, 1002);	// 16: output result
+	//asm_cmd(HLT, 0, 0, 0, 0);		// 17: halt
 	/* Multiplication table program starts here
 	   R2 = row
 	   R3 = column
@@ -92,36 +92,36 @@ static void assemble_program(char *program_name)
 	   R6 = memory boundary
 	*/
 
-	/*asm_cmd(ADD, 2, 0, 1, 10);		// 0:  row = 10
+	asm_cmd(ADD, 2, 0, 1, 10);		// 0:  row = 10
 	asm_cmd(ADD, 3, 0, 1, 10);		// 1:  column = 10
 	asm_cmd(ADD, 5, 0, 1, 2099);	// 2:  ptr = 2099
 	asm_cmd(ADD, 6, 0, 1, 10);		// 3:  saved_column = 10
-	// loop:
+	 //loop:
 	asm_cmd(JEQ, 0, 2, 0, 18);		// 4:  Exit condition
 	asm_cmd(ADD, 4, 0, 1, 0);		// 5:  result = 0
-	// mult:
+	 //mult:
 	asm_cmd(JEQ, 0, 3, 0, 10);		// 6:  if (column == 0) goto store
 	asm_cmd(ADD, 4, 4, 2, 0);		// 7:  result += row
 	asm_cmd(SUB, 3, 3, 1, 1);		// 8:  column -= 1;
 	asm_cmd(JEQ, 0, 0, 0, 6);		// 9:  goto mult
-	// store:
+	 //store:
 	asm_cmd(ST,  0, 4, 5, 0);		// 10: mem[ptr] = result
 	asm_cmd(SUB, 5, 5, 1, 1);		// 11: ptr -= 1
 	asm_cmd(SUB, 6, 6, 1, 1);		// 12: saved_column -= 1
 	asm_cmd(JNE, 0, 6, 0, 16);		// 13: if (saved_column != 0) goto restore
 	asm_cmd(ADD, 6, 0, 1, 10);		// 14: saved_column = 10
 	asm_cmd(SUB, 2, 2, 1, 1);		// 15: row -= 1
-	// restore:
+	 //restore:
 	asm_cmd(ADD, 3, 6, 0, 0);		// 16: column = saved_column
 	asm_cmd(JEQ, 0, 0, 0, 4);		// 17: goto test
-	asm_cmd(HLT, 0, 0, 0, 0);		// 18: halt*/
+	asm_cmd(HLT, 0, 0, 0, 0);		// 18: halt
 
 	// Original program memory init
 	//for (i = 0; i < 5; i++)
 	//	mem[15+i] = i;
 	// Multiplication program memory init
-	mem[1000] = -3;
-	mem[1001] = -5;
+	/*mem[1000] = -3;
+	mem[1001] = -5;*/
 
 	// Original program memory init
 	//last_addr = 20;
