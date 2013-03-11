4626c14c22406bbea64a66308e05de64087ab120
Shay Barak <shiber@gmail.com>
2013-03-11 16:59:39 +0200

    Multiplication table program

--- asm.c
+++ asm.c
@@ -62,6 +62,7 @@ static void assemble_program(char *program_name)
 	asm_cmd(ST,  0, 3, 1, 15); // 7: MEM[15] = R3
 	asm_cmd(HLT, 0, 0, 0, 0);  // 8: Halt
 	*/
+	/* Multiplication program starts here
 	asm_cmd(LD,  2, 0, 1, 1000);	// 0:  Load multiplicand
 	asm_cmd(LD,  3, 0, 1, 1001);	// 1:  Load multiplier
 	asm_cmd(ADD, 4, 0, 0, 0);		// 2:  SUM = 0
@@ -78,17 +79,51 @@ static void assemble_program(char *program_name)
 	asm_cmd(JEQ, 0, 0, 0, 8);		// 11: goto loop_negative
 	// done:
 	asm_cmd(ST,  0, 4, 1, 1002);	// 12: output result
-	asm_cmd(HLT, 0, 0, 0, 0);
+	asm_cmd(HLT, 0, 0, 0, 0);*/
+	/* Multiplication table program starts here
+	   R2 = row
+	   R3 = column
+	   R4 = result
+	   R5 = memory pointer
+	   R6 = memory boundary
+	*/
+
+	asm_cmd(ADD, 2, 0, 1, 10);		// 0:  row = 10
+	asm_cmd(ADD, 3, 0, 1, 10);		// 1:  column = 10
+	asm_cmd(ADD, 5, 0, 1, 2099);	// 2:  ptr = 2099
+	asm_cmd(ADD, 6, 0, 1, 10);		// 3:  saved_column = 10
+	// loop:
+	asm_cmd(JEQ, 0, 2, 0, 18);		// 4:  Exit condition
+	asm_cmd(ADD, 4, 0, 1, 0);		// 5:  result = 0
+	// mult:
+	asm_cmd(JEQ, 0, 3, 0, 10);		// 6:  if (column == 0) goto store
+	asm_cmd(ADD, 4, 4, 2, 0);		// 7:  result += row
+	asm_cmd(SUB, 3, 3, 1, 1);		// 8:  column -= 1;
+	asm_cmd(JEQ, 0, 0, 0, 6);		// 9:  goto mult
+	// store:
+	asm_cmd(ST,  0, 4, 5, 0);		// 10: mem[ptr] = result
+	asm_cmd(SUB, 5, 5, 1, 1);		// 11: ptr -= 1
+	asm_cmd(SUB, 6, 6, 1, 1);		// 12: saved_column -= 1
+	asm_cmd(JNE, 0, 6, 0, 16);		// 13: if (saved_column != 0) goto restore
+	asm_cmd(ADD, 6, 0, 1, 10);		// 14: saved_column = 10
+	asm_cmd(SUB, 2, 2, 1, 1);		// 15: row -= 1
+	// restore:
+	asm_cmd(ADD, 3, 6, 0, 0);		// 16: column = saved_column
+	asm_cmd(JEQ, 0, 0, 0, 4);		// 17: goto test
+	asm_cmd(HLT, 0, 0, 0, 0);		// 18: halt
 
 	// Original program memory init
 	//for (i = 0; i < 5; i++)
 	//	mem[15+i] = i;
-	mem[1000] = 5;
-	mem[1001] = -2;
+	// Multiplication program memory init
+	//mem[1000] = 5;
+	//mem[1001] = -2;
 
 	// Original program memory init
 	//last_addr = 20;
-	last_addr = 1002;
+	// Multiplication program memory init
+	//last_addr = 1002;
+	last_addr = 2100;
 
 	fp = fopen(program_name, "w");
 	if (fp == NULL) {
